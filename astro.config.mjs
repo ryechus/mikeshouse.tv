434db0d071c5db5b4f6670eaae99d8439b40eba1
MJ Hardin <mhardinla@gmail.com>
2026-04-22 08:47:59 -0700

    v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mikeshouse.tv",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap()],
+});
