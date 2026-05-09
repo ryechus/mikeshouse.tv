4ebdb315f4fd6951c532869231cbbaf921742fc5
MJ Hardin <mhardinla@gmail.com>
2026-05-08 19:12:36 -0700

    productionized

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,12 @@
 export const siteTitle = "Mike's House";
+
+let tempBackendUrl = "http://localhost:8000";
+let tempSiteUrl = "http://localhost:4321";
+
+if (import.meta.env.MODE === "production") {
+  tempBackendUrl = "https://api.mikeshouse.tv";
+  tempSiteUrl = "https://mikeshouse.tv";
+}
+
+export const backendUrl = tempBackendUrl;
+export const siteUrl = tempSiteUrl;
